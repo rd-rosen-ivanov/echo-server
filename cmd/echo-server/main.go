020f0686682766177c3b622b0a78741758100374
Rosen Ivanov <rosen.ivanov.extern@rewe-digital.com>
2022-01-10 15:01:57 +0200

    Star server mocked

--- cmd/echo-server/main.go
+++ cmd/echo-server/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
@@ -20,7 +21,7 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8090"
 	}
 
 	fmt.Printf("Echo server listening on port %s.\n", port)
@@ -151,17 +152,27 @@ func serveWebSocket(wr http.ResponseWriter, req *http.Request, sendServerHostnam
 }
 
 func serveHTTP(wr http.ResponseWriter, req *http.Request, sendServerHostname bool) {
-	wr.Header().Add("Content-Type", "text/plain")
-	wr.WriteHeader(200)
+	//wr.Header().Add("Content-Type", "text/plain")
+	wr.Header().Add("Content-Type", "application/json")
+	//wr.WriteHeader(200)
+	randomFail := rand.Int()%2 != 0
 
-	if sendServerHostname {
-		host, err := os.Hostname()
-		if err == nil {
-			fmt.Fprintf(wr, "Request served by %s\n\n", host)
-		} else {
-			fmt.Fprintf(wr, "Server hostname unknown: %s\n\n", err.Error())
-		}
+	if randomFail {
+		wr.WriteHeader(403)
+		fmt.Println("return 403")
+	} else {
+		fmt.Println("return 200")
+		wr.WriteHeader(200)
 	}
+
+	// if sendServerHostname {
+	// 	host, err := os.Hostname()
+	// 	if err == nil {
+	// 		fmt.Fprintf(wr, "Request served by %s\n\n", host)
+	// 	} else {
+	// 		fmt.Fprintf(wr, "Server hostname unknown: %s\n\n", err.Error())
+	// 	}
+	// }
 
 	writeRequest(wr, req)
 }
@@ -253,18 +264,22 @@ func writeSSEField(
 
 // writeRequest writes request headers to w.
 func writeRequest(w io.Writer, req *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", req.Proto, req.Method, req.URL)
-	fmt.Fprintln(w, "")
+	// fmt.Fprintf(w, "%s %s %s\n", req.Proto, req.Method, req.URL)
+	// fmt.Fprintln(w, "")
 
-	fmt.Fprintf(w, "Host: %s\n", req.Host)
-	for key, values := range req.Header {
-		for _, value := range values {
-			fmt.Fprintf(w, "%s: %s\n", key, value)
-		}
-	}
+	// fmt.Fprintf(w, "Host: %s\n", req.Host)
+	// for key, values := range req.Header {
+	// 	for _, value := range values {
+	// 		fmt.Fprintf(w, "%s: %s\n", key, value)
+	// 	}
+	// }
+	// println("about to sleep")
+	// time.Sleep(5 * time.Second)
+	// println("slept 5 seconds")
 
 	var body bytes.Buffer
-	io.Copy(&body, req.Body) // nolint:errcheck
+	//io.Copy(&body, bytes.NewReader([]byte("{\"access_token\": \"eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJGTzFNRUhJWGpWSklMMkptSm5xWkRYQllWdzJTOHk3MElqZ0pPSFpMakZFIn0.eyJleHAiOjE2MjY3ODEzMzYsImlhdCI6MTYyNjc4MTAzNiwiYXV0aF90aW1lIjoxNjI2NzgwOTYyLCJqdGkiOiI2NTQ2MTNhNi05ZjFjLTQ3MzUtODEwMy1jMzZhNzE4NmEzNjgiLCJpc3MiOiJodHRwczovL2F1dGgtc2VydmljZS5zZXJ2aWNlLmRldi5mZnAtZGV2LmNvbS9hdXRoL3JlYWxtcy9yZXdlLWZ1bGZpbGxtZW50IiwiYXVkIjoibG9jYXRpb24tY29ubmVjdG9yLWJhY2tvZmZpY2UiLCJzdWIiOiI0OWIyY2Q5Ny02YjNkLTRjODAtYTY0Ny0zYTYxOWU1OWUxZGIiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJsb2NhdGlvbi1iYWNrb2ZmaWNlIiwibm9uY2UiOiJOMC4wODc1MzM5ODI1OTAyOTEyMTE2MjY3ODEwMzYyNTQiLCJzZXNzaW9uX3N0YXRlIjoiMzBkYThhZTgtMjBjYy00MmM1LWIxYzItNTdhOWI5MTBjMmY4IiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vbG9jYWxob3N0OjQyMDAiLCJodHRwczovL3Jld2Uuc2VydmljZS5kZXYuZmZwLWRldi5jb20iXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIlNSLWxvY2F0aW9uLWNvbm5lY3Rvci1iYWNrb2ZmaWNlLXNlcnZpY2Vsb2NhdGlvbnMtYWRtaW4iLCJTUi1sb2NhdGlvbi1jb25uZWN0b3ItYmFja29mZmljZS1hZG1pbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImxvY2F0aW9uLWNvbm5lY3Rvci1iYWNrb2ZmaWNlIjp7InJvbGVzIjpbImRvd25sb2FkX2xvY2F0aW9ucyIsImluaXRpYWxpemUtd2FyZWhvdXNlcyIsInVwbG9hZF9sb2NhdGlvbnMiLCJkZWxldGVfbG9jYXRpb25zIiwiZWJvb3N0ZXItaW1wb3J0Iiwidmlld19sb2NhdGlvbnMiLCJ1cGxvYWRfbG9jYXRpb25zX2RpbWVuc2lvbnMiLCJjcmVhdGVfbG9jYXRpb25zIiwiYWRtaW5fc2VydmljZV9sb2NhdGlvbnMiXX0sImxvY2F0aW9uLWJhY2tvZmZpY2UiOnsicm9sZXMiOlsiYmFja29mZmljZS1wb3J0YWwtdmlldy1hcHAiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIHN0YW5kYXJkIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJsb2NhdGlvbnMiOlsiMjMxMDA2fEJlcmxpbjEiLCIyMzEwMTF8TcO8bmNoZW4iLCIyNDA1NTd8QmVybGluIDMiLCIyNDA1ODF8U3R1dHRnYXJ0IiwiMzIwNTA5fEZGQyAyLjAiLCIzMjA1MTZ8TWFubmhlaW0iXSwicHJlZmVycmVkX3VzZXJuYW1lIjoidGVhbS1hbHBoYSIsImxvY2FsZSI6ImRlIiwiZW1haWwiOiJkZXYtdGVhbS1hbHBoYUByZXdlLWRpZ2l0YWwuY29tIn0.CIdYP8l3kTfrkq0bv5daFjRKHxenuG60YBpbEkvpLiAmLqqZ8ZIohuO2SGA0UEHH7TBHS0gzae6Za0bapYq8dlOXDTEoTIn0439WkM6r2CZsrNBPN8T4aV7OeGxmDvzuRgltG2tTRHNmrFWYt8KrHu7SgNrX0lg990wHQgLitr1t_QihdZCSidMiUez8uUbVBBhB3qFyrshHF9OLzrfWIkkbI8TwS_HE-QAIxotnB5y3eV0rDRZGNHQ6SUt9UWoZ8G2fm_rNDMNEOsNmsRYIdQMF8wv4BcIZoftpaC3TlKB4UoZbg1Zk5QYYPl6neUandpIxIa1TGDcGc_5EOD42zQ\"}"))) // nolint:errcheck
+	io.Copy(&body, bytes.NewReader([]byte("{\"access_token\": \"fake-token\", \"gtins\": [\"999\"], \"changesUntil\": \"2020-12-12T00:00:11.111Z\"}"))) // nolint:errcheck
 
 	if body.Len() > 0 {
 		fmt.Fprintln(w, "")
